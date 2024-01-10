d82a459a5ed9efbdc706386a2a4621669001e31e
Naman Nangia <96681933+namannangia@users.noreply.github.com>
2024-01-10 20:50:25 +0000

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,177 @@
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { dataset } from "./dataset";
 function App() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+  const [rightVal, setRightVal] = React.useState(window.innerWidth / 2 + 550);
+  const [slRange, setSlRange] = React.useState(0);
+  const [rgbVal, setRgbVal] = React.useState("17,57,70");
+  React.useEffect(() => {
+    if (slRange !== 0)
+      setRgbVal(
+        Math.random(slRange) * 100 +
+          "," +
+          Math.random(slRange + 1) * 100 +
+          "," +
+          Math.random(slRange + 2) * 100
+      );
+    console.log(slRange, rgbVal);
+  }, [slRange]);
+  const styleSheet = {
+    root: {
+      display: "flex",
+      height: "100vh",
+      width: "100vw",
+      // backgroundColor: "#113946",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    main: {
+      display: "flex",
+      padding: "5px",
+      borderRadius: "20px",
+      height: "400px",
+      width: "430px",
+      backgroundColor: "rgba(255,255,255,0.2)",
+      boxShadow: "0px 0px 20px 10px rgba(255,255,255,0.1)",
+      flexDirection: "column",
+      alignItems: "center",
+      overflow: "hidden",
+    },
+    contentDiv: {
+      display: "flex",
+      flexDirection: "row",
+      flex: 8,
+      borderRadius: "20px",
+      fontSize: "18px",
+      color: "white",
+      padding: "20px",
+      overflow: "hidden",
+      // border: "2px solid red",s
+    },
+    innerContentDiv: {
+      display: "flex",
+      flex: 8,
+      color: "rgba(255,255,255,0.8)",
+      borderRadius: "20px",
+      flexDirection: "column",
+    },
+    btnHolderDiv: {
+      display: "flex",
+      flex: 1,
+      borderRadius: "20px",
+      justifyContent: "center",
+      alignItems: "center",
+      padding: "10px",
+    },
+    mainBtn: {
+      width: "10px",
+      height: "10px",
+      borderRadius: "20px",
+      justifyContent: "center",
+      alignItems: "center",
+      margin: "10px",
+    },
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div style={{ backgroundColor: `rgb(${rgbVal})`, ...styleSheet.root }}>
+      <div
+        style={{
+          position: "absolute",
+          borderRadius: "0px 40px 40px 0px",
+          top: "10px",
+          left: "0px",
+          padding: " 10px 20px",
+          color: "white",
+          backgroundColor: "rgba(255,255,255,0.5)",
+        }}
+      >
+        <button
+          style={{
+            padding: "10px",
+            color: "rgb(" + rgbVal + ")",
+            fontWeight: "700",
+            backgroundColor: "rgba(255,255,255,0.7)",
+          }}
+          onClick={() => {
+            setSlRange(Math.random());
+          }}
+        >
+          Random theme
+        </button>
+        <button
+          style={{
+            padding: "10px",
+            color: "rgb(" + rgbVal + ")",
+            fontWeight: "700",
+            backgroundColor: "rgba(255,255,255,0.7)",
+            marginLeft: "20px",
+          }}
+          onClick={() => {
+            window.location.href = "https://www.google.com";
+          }}
+        >
+          Source Code
+        </button>
+      </div>
+      <div style={styleSheet.main}>
+        <div
+          style={{
+            ...styleSheet.contentDiv,
+          }}
+        >
+          {dataset.map((a, i) => {
+            return (
+              <div
+                key={i}
+                style={{
+                  position: "relative",
+                  left: rightVal,
+                  width: "400px",
+                  textAlign: "center",
+                  margin: "20px",
+                }}
+              >
+                <p>{a.ques}</p>
+                <h4 style={{ textAlign: "end" }}>— {a.ans}</h4>
+              </div>
+            );
+          })}
+        </div>
+        <div style={styleSheet.btnHolderDiv}>
+          {dataset.map((a, i) => {
+            return (
+              <div
+                onClick={() => {
+                  if (i > activeIndex) {
+                    var z = i;
+                    while (z !== activeIndex) {
+                      z -= 1;
+                      setRightVal((e) => e - 440);
+                    }
+                  } else if (i < activeIndex) var z = i;
+                  while (z !== activeIndex) {
+                    z += 1;
+                    setRightVal((e) => e + 440);
+                  }
+                  setActiveIndex(i);
+                }}
+                style={{
+                  backgroundColor:
+                    activeIndex !== i
+                      ? "rgba(255,255,255,0.5)"
+                      : "rgba(0,0,0,0.5)",
+                  cursor: "pointer",
+                  padding: "3px",
+                  border:
+                    activeIndex === i ? "2px solid rgba(255,255,255,0.6)" : "",
+                  ...styleSheet.mainBtn,
+                }}
+              ></div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/dataset.js
@@ -0,0 +1,30 @@
+export const dataset = [
+  {
+    ques: "Born in 1955, this tech visionary co-founded a renowned computer company in 1976. His innovative products revolutionized the industry, introducing iconic devices like the Macintosh and iPhone. Who is this trailblazer?",
+    ans: "Steve Jobs",
+  },
+  {
+    ques: "A Nobel laureate in physics, this scientist's groundbreaking work in quantum mechanics and electromagnetism laid the foundation for modern technology. Can you identify this brilliant mind who reshaped our understanding of the physical world?",
+    ans: "Albert Einstien",
+  },
+  {
+    ques: "Hailing from South Africa, this entrepreneur and inventor is known for his advancements in electric vehicles and sustainable energy solutions. With a company named after him, who is this forward-thinking individual shaping the future of transportation?",
+    ans: "Elon Musk",
+  },
+  {
+    ques: "An iconic figure in the world of literature, this author penned numerous timeless classics that continue to captivate readers worldwide. Recognized for his wit and insight, who is this literary genius behind works like 1984 and Animal Farm?Born in 1971, this talented actor rose to fame with roles in critically acclaimed films",
+    ans: "George Orwell",
+  },
+  {
+    ques: "Born in 1971, this talented actor rose to fame with roles in critically acclaimed films such as 'The Wolf of Wall Street' and 'Inception'. Who is this versatile performer, widely regarded for his dedication to his craft and compelling on-screen presence?With a career spanning over five decades, this legendary musician is celebrated for ",
+    ans: "Leonardo DiCaprio",
+  },
+  {
+    ques: "With a career spanning over five decades, this legendary musician is celebrated for his influential contributions to rock and roll. Known for hits like 'Stairway to Heaven', can you identify this iconic guitarist and songwriter?",
+    ans: "Jimmy Page",
+  },
+  {
+    ques: "This trailblazing athlete, born in 1984, has dominated the world of tennis with an unparalleled skill set and numerous Grand Slam victories. Who is this tennis superstar, often regarded as one of the greatest players of all time?",
+    ans: "Serena Williams",
+  },
+];
